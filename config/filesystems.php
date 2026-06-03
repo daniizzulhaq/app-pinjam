0a9240080bf9bc5171b3a9ba20087cacd58eb472
daniizzulhaq <daniizzulhaq2002tkj@gmail.com>
2026-06-04 01:25:50 +0700

    fix fylesistem

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,14 +38,14 @@ return [
             'report' => false,
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
-            'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
-        ],
+       'public' => [
+    'driver' => 'local',
+    'root' => '/home/pinjamin/public_html/storage',
+    'url' => env('APP_URL').'/storage',
+    'visibility' => 'public',
+    'throw' => false,
+    'report' => false,
+],
 
         's3' => [
             'driver' => 's3',
